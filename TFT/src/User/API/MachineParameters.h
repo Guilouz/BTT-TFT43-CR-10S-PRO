e8688bc623725e77d7ef50a7122f81ad3b93d51d
Cyril <guilouz6@hotmail.com>
2021-02-13 00:46:03 +0100

    Update 13-02-2021

--- TFT/src/User/API/MachineParameters.h
+++ TFT/src/User/API/MachineParameters.h
@@ -27,7 +27,6 @@ typedef enum
   P_CURRENT,
   P_BUMPSENSITIVITY,
   P_HYBRID_THRESHOLD,
-  P_STEALTH_CHOP,
   P_MBL_OFFSET,
   // Keep below items always at the end
   P_SAVE_SETTINGS,
@@ -75,7 +74,6 @@ typedef struct
   float Current[STEPPER_COUNT];
   float BumpSensitivity[3];
   float HybridThreshold[STEPPER_COUNT];
-  float StealthChop[5];
   float MblOffset[1];
 } PARAMETERS;
 
